f92566ae75591600c6a84dd8432f990ef478f4c4
treekey999 <treekey999@gmail.com>
2019-06-13 14:18:17 +0800

    add before_action check and recaptcha form tag

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
+  before_action :check_recaptcha_v3, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -24,4 +25,20 @@ class Users::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  private
+
+  def check_recaptcha_v3
+    valid = verify_recaptcha(
+      # action：當收到 recaptcha token 的時候要認哪一個名稱來做驗證
+      #         與前端 form 表單呼應
+      action: 'login',
+      minimum_score: 0.5,
+      secret_key: ENV["RECAPTCHA_V3_SECRET_KEY"],
+    )
+
+    if not valid
+      redirect_to new_user_session_path_path
+    end
+  end
 end
